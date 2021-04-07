b9e3dd6754009e254fb9d8b6b03a15e211000226
SinanUzunal <81981238+SinanUzunal@users.noreply.github.com>
2021-04-07 19:34:59 +0300

    Update HW1.py

--- Homeworks/HW1.py
+++ Homeworks/HW1.py
@@ -1,5 +1,29 @@
-#Explain your work
-
 #Question 1
-for x in range(a):
-	print(a)
+list1 =[1,2,3,4,5,6,7,8,9,10]
+print("Original List = ",list1)
+list2 = list()
+for i in range(1,(len(list1)//2)+1):
+  list2.append(list1[(-1)])
+  list1.pop()
+list2.reverse()
+list2.extend(list1)
+list1[:] = []
+list1.extend(list2)
+print("Final List = ",list1)   
+
+#Question 2
+control=True
+while control :
+  n=int(input("Please input a single digit integer: "))
+  if 0<n and n<10:
+    for i in range(n+1):
+      if (i%2==0):
+        print(i)
+    control=False
+  elif 0>n and n>-10:
+    for i in range(0,n-1,-1):
+      if (i%2==0):
+       print(i)
+    control=False
+  else:
+    print("ERROR! Number is not a single digit integer!\n")
